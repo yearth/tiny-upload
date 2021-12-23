3ac8499d20265e24cbf3fb756fd85e6ad0687e00
yearthmain <yearthmain@gmail.com>
2021-12-23 23:02:10 +0800

    feat: v1

--- back-end/entry.js
+++ back-end/entry.js
@@ -1,12 +1,30 @@
 const Koa = require("koa");
+const koaBody = require("koa-body");
+const fsEx = require("fs-extra");
+const path = require("path");
 
 const app = new Koa();
 
-app.use(() => {
+const UPLOAD_DIR = path.resolve(__dirname, "static");
+
+app.use(
+  koaBody({
+    multipart: true
+  })
+);
+
+app.use(ctx => {
   const { url, query } = ctx.request;
 
-  if (url === "/") {
-    ctx.body = "hello world";
+  if (url === "/upload") {
+    const { file } = ctx.request.files;
+    const filename = file.name;
+    const cachePath = file.path;
+    const targetPath = `${UPLOAD_DIR}/${filename}`;
+
+    fsEx.move(cachePath, targetPath);
+
+    // ctx.body = "hello world";
   }
 });
 
